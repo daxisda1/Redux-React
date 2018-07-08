de18d6bb9339ae7b12ad5f967da62c283c0c889b
Dane Shambrook <32531869+daxisda1@users.noreply.github.com>
2018-07-08 15:54:29 -0700

    Add files via upload

---
+++ src/screens/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { render } from 'react-dom';
+import Search from './src/screens/Search';
+import Mine from './src/screens/Mine';
+import Home from './src/screens/Home';
+
+export { default as Home } from './Home';
+export { default as Mine } from './Mine';
+export { default as Search } from './Search';
+
+import { Home, Mine, Search } from './screens';
+ServiceWorkerRegistration ();
